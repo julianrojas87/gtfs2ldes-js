c37094b21ba8447285cc59b36d96aff556f9a5a5
Julian Rojas <julian.rojas87@gmail.com>
2022-07-08 14:18:34 +0200

    Add testing for GTFS-realtime processing

--- test/gtfs2ldes.test.js
+++ test/gtfs2ldes.test.js
@@ -1,8 +1,10 @@
 import { jest } from "@jest/globals";
-import fastify from "fastify";
 import fs from "fs";
+import fastify from "fastify";
+import { Level } from "level";
 import del from "del";
 import { processGTFS } from "../lib/GTFS.js";
+import { buildIndexes, processGTFSRealtime } from "../lib/GTFSRealtime.js";
 
 // Mock config
 const config = {
@@ -12,6 +14,7 @@ const config = {
         "throttle_rate": 5
     },
     "gtfs": {},
+    "gtfs_realtime": {},
     "@context": {
         "xsd": "http://www.w3.org/2001/XMLSchema#",
         "dct": "http://purl.org/dc/terms/",
@@ -47,6 +50,8 @@ const config = {
 jest.setTimeout(30000);
 
 let server;
+let historyDB;
+let indexes;
 
 beforeAll(async () => {
     // Setup mock target server
@@ -56,26 +61,80 @@ beforeAll(async () => {
             reply.send("OK");
         });
     });
-    
+
     await server.listen({ port: 8080, host: "0.0.0.0" });
 });
 
-test("Process GTFS file for the first time (should produce 60 connections)", async () => {
+test("Process GTFS file for the first time (should produce a total of 201 connections)", async () => {
     expect.assertions(2);
     config["gtfs"].source = "./test/data/delijn.test.0.zip";
     const { count, failed } = await processGTFS(config);
-    expect(count).toBe(60);
+    expect(count).toBe(201);
     expect(failed).toBe(0);
 });
 
-test("Process second GTFS file which should only produce 2 new connections", async () => {
+test("Process second GTFS file (should only produce 6 new connections due to historic records)", async () => {
     expect.assertions(2);
     config["gtfs"].source = "./test/data/delijn.test.1.zip";
     const { count, failed } = await processGTFS(config);
-    expect(count).toBe(2);
+    expect(count).toBe(6);
     expect(failed).toBe(0);
+});
+
+test("Process first GTFS-realtime update (should update only 1 connection with delays)", async () => {
+    expect.assertions(4);
+    // Build static indexes
+    const idxs = await getIndexes();
+    const history = await getHistoryDB();
+    idxs.historyDB = history;
+    // Process GTFS-realtime update
+    config["gtfs_realtime"].source = "./test/data/delijn-realtime.0.pbf";
+    const { count, failed } = await processGTFSRealtime(config, idxs);
+    // Get connection we expect to be updated
+    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:01:00/15:03:00/0/0");
+
+    expect(count).toBe(1);
+    expect(failed).toBe(0);
+    expect(updatedConn['20220707'].departureDelay).toBe(713);
+    expect(updatedConn['20220707'].arrivalDelay).toBe(713);
+});
+
+test("Process second GTFS-realtime update (should update only 1 connection with different delays)", async () => {
+    expect.assertions(4);
+    // Build static indexes
+    const idxs = await getIndexes();
+    const history = await getHistoryDB();
+    idxs.historyDB = history;
+    // Process GTFS-realtime update
+    config["gtfs_realtime"].source = "./test/data/delijn-realtime.1.pbf";
+    const { count, failed } = await processGTFSRealtime(config, idxs);
+    // Get connection we expect to be updated
+    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:01:00/15:03:00/0/0");
+    
+    expect(count).toBe(1);
+    expect(failed).toBe(0);
+    expect(updatedConn['20220707'].departureDelay).toBe(744);
+    expect(updatedConn['20220707'].arrivalDelay).toBe(744);
     await cleanUp();
 });
+
+// Function to get set of static indexes
+async function getIndexes() {
+    if(!indexes) {
+        indexes = await buildIndexes(config);
+    }
+
+    return indexes;
+}
+
+// Function to get reference to historic records
+async function getHistoryDB() {
+    if(!historyDB) {
+        historyDB = await new Level("./test/data/history.db", { valueEncoding: "json" });
+    }
+    await historyDB.open();
+    return historyDB;
+}
 
 async function cleanUp() {
     await del([
