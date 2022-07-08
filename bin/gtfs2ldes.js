87989265ff2f5feb895bfa113ee21b82c655fc7e
Julian Rojas <julian.rojas87@gmail.com>
2022-07-08 20:34:36 +0200

    Add checks for missing sources

--- bin/gtfs2ldes.js
+++ bin/gtfs2ldes.js
@@ -15,6 +15,7 @@ async function run() {
     // Create cron job to process GTFS-realtime updates
     const gtfsRealtimeJob = new CronJob({
         cronTime: config["gtfs_realtime"].cron,
+        start: true,
         onTick: async () => {
             if (!indexes) {
                 const t0 = new Date();
@@ -23,15 +24,19 @@ async function run() {
                 console.log("Building indexes for GTFS-realtime processing...");
                 indexes = "building";
                 indexes = await buildIndexes(config);
-                // Retrieve reference to historic connections index
-                const historyDB = new Level(`${config["general"].data_folder}/history.db`, { valueEncoding: "json" });
-                await historyDB.open();
-                indexes.historyDB = historyDB;
-                console.log(`Indexes for GTFS-realtime processing built in ${new Date() - t0} ms`);
-                // Start processing latest realtime update
-                rtState = true;
-                await processGTFSRealtime(config, indexes);
-                rtState = false;
+                if (indexes) {
+                    // Retrieve reference to historic connections index
+                    const historyDB = new Level(`${config["general"].data_folder}/history.db`, { valueEncoding: "json" });
+                    await historyDB.open();
+                    indexes.historyDB = historyDB;
+                    console.log(`Indexes for GTFS-realtime processing built in ${new Date() - t0} ms`);
+                    // Start processing latest realtime update
+                    rtState = true;
+                    await processGTFSRealtime(config, indexes);
+                    rtState = false;
+                } else {
+                    console.warn("No GTFS sources found yet. Consider running the application with RUN_ON_LAUNCH=true to process realtime updates");
+                }
             } else if (indexes !== "building" && !rtState) {
                 // We have indexes and we are not processing an update already
                 rtState = true;
@@ -44,6 +49,7 @@ async function run() {
     // Create and schedule cron job to process static GTFS
     new CronJob({
         cronTime: config["gtfs"].cron,
+        start: true,
         onTick: async () => {
             // Stop realtime process first
             if (gtfsRealtimeJob.running) {
@@ -54,14 +60,15 @@ async function run() {
             indexes = null;
             // Schedule realtime job now that we processed static data
             gtfsRealtimeJob.start();
-        },
-        start: true
+        }
     });
 
     // Trigger data jobs if configured for it
-    if (config["general"].run_on_launch) {
+    if (config["general"].run_on_launch === "true") {
+        // Temporarily stop GTFS-realtime job
+        gtfsRealtimeJob.stop();
         // Process static GTFS source
-        //await processGTFS(config);
+        await processGTFS(config);
         // Kick-off GTFS-realtime job
         gtfsRealtimeJob.start();
     }
