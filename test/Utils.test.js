f8ce16c767067714e1e2db85e33e7bc95ada75e8
Julian Rojas <julian.rojas87@gmail.com>
2023-12-11 22:45:36 +0100

    Changes include: - update dependencies - remove unzipper in favor of adm-zip

--- test/Utils.test.js
+++ test/Utils.test.js
@@ -1,9 +1,27 @@
+import { jest, test, expect, afterEach } from "@jest/globals";
+import { deleteAsync } from "del";
+import fs from "fs";
 import Utils from "../lib/Utils";
 
+jest.setTimeout(15000);
+
 test("Download function test", async () => {
-    expect.assertions(1);
     const response = await Utils.download("https://api.delijn.be/gtfs/v1/realtime", {
         "Ocp-Apim-Subscription-Key": "cf97c65afbe84e0c8e20142f4cb62119"
     });
-    expect(response).toBeDefined();
+    expect(response).toBe("/tmp/gtfs.zip");
+});
+
+test("Unzip function from URL", async () => {
+    const path = await Utils.download("https://www.rtd-denver.com/files/gtfs/bustang-co-us.zip");
+    expect(path).toBe("/tmp/gtfs.zip");
+    Utils.unzip(path, "/tmp/unzipped");
+    expect(fs.existsSync("/tmp/unzipped")).toBeTruthy();
+});
+
+afterEach(async () => {
+    await deleteAsync([
+        "/tmp/gtfs.zip",
+        "/tmp/unzipped"
+    ], { force: true });
 });
--- test/gtfs2ldes.test.js
+++ test/gtfs2ldes.test.js
@@ -1,8 +1,7 @@
-import { jest } from "@jest/globals";
-import fs from "fs";
+import { jest, test, beforeAll, afterAll, expect } from "@jest/globals";
 import fastify from "fastify";
 import { Level } from "level";
-import del from "del";
+import { deleteAsync } from "del";
 import { processGTFS } from "../lib/GTFS.js";
 import { buildIndexes, processGTFSRealtime } from "../lib/GTFSRealtime.js";
 
@@ -57,11 +56,11 @@ const config = {
     }
 };
 
-jest.setTimeout(30000);
-
 let server;
 let historyDB;
 let indexes;
+
+jest.setTimeout(30000);
 
 beforeAll(async () => {
     // Setup mock target server
@@ -86,7 +85,6 @@ afterAll(async () => {
 });
 
 test("Process GTFS file for the first time (should produce a total of 201 connections)", async () => {
-    expect.assertions(2);
     config["gtfs"]["source"] = "./test/data/delijn.test.0.zip";
     const { count, failed } = await processGTFS(config);
     expect(count).toBe(201);
@@ -95,7 +93,6 @@ test("Process GTFS file for the first time (should produce a total of 201 connec
 });
 
 test("Process second GTFS file (should only produce 6 new connections due to historic records)", async () => {
-    expect.assertions(2);
     config["gtfs"]["source"] = "./test/data/delijn.test.1.zip";
     const { count, failed } = await processGTFS(config);
     expect(count).toBe(6);
@@ -104,7 +101,6 @@ test("Process second GTFS file (should only produce 6 new connections due to his
 });
 
 test("Process first GTFS-realtime update (should update only 1 connection with delays)", async () => {
-    expect.assertions(4);
     // Build static indexes
     const idxs = await getIndexes();
     const history = await getHistoryDB();
@@ -113,7 +109,7 @@ test("Process first GTFS-realtime update (should update only 1 connection with d
     config["gtfs_realtime"]["source"] = "./test/data/delijn-realtime.0.pbf";
     const { count, failed } = await processGTFSRealtime(config, idxs);
     // Get connection we expect to be updated
-    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:01:00/15:03:00/0/0");
+    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:04:00/15:05:00/0/0");
 
     expect(count).toBe(1);
     expect(failed).toBe(0);
@@ -122,7 +118,6 @@ test("Process first GTFS-realtime update (should update only 1 connection with d
 });
 
 test("Process second GTFS-realtime update (should update only 1 connection with different delays)", async () => {
-    expect.assertions(4);
     // Build static indexes
     const idxs = await getIndexes();
     const history = await getHistoryDB();
@@ -131,7 +126,7 @@ test("Process second GTFS-realtime update (should update only 1 connection with 
     config["gtfs_realtime"]["source"] = "./test/data/delijn-realtime.1.pbf";
     const { count, failed } = await processGTFSRealtime(config, idxs);
     // Get connection we expect to be updated
-    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:01:00/15:03:00/0/0");
+    const updatedConn = await history.get("Turnhout-Herentals-Herselt-Leuven/59/102827/106231/13:10:00/15:04:00/15:05:00/0/0");
 
     expect(count).toBe(1);
     expect(failed).toBe(0);
@@ -159,11 +154,11 @@ async function getHistoryDB() {
 }
 
 async function cleanOutput() {
-    await del(["./test/data/linkedConnections.json.gz"]);
+    await deleteAsync(["./test/data/linkedConnections.json.gz"]);
 }
 
 async function cleanUp() {
-    await del([
+    await deleteAsync([
         "./test/data/*.txt",
         "./test/data/history.db",
         "./test/data/linkedConnections.json.gz"
